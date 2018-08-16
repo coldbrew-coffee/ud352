7286c8c5c277ed76a6262bf9debc7f9f8402ebae
Nicholas Kim <nicholaskim94@gmail.com>
2018-08-16 21:10:01 +0900

    Finish Project Sandbox Playground

---
+++ Sandbox Playground/Sandbox Playground/AppDelegate.swift
@@ -0,0 +1,51 @@
+//
+//  AppDelegate.swift
+//  SandboxPlayground
+//
+//  Created by Fernando Rodríguez Romero on 13/05/16.
+//  Copyright © 2016 udacity.com. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - AppDelegate: UIResponder, UIApplicationDelegate
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  
+  // MARK: Properties
+  
+  var window: UIWindow?
+  
+  // MARK: UIApplicationDelegate
+  
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    sandboxPlayground()
+    return true
+  }
+  
+  func sandboxPlayground() {
+    let fileManager = FileManager.default
+    let urls = fileManager.urls(for: .documentDirectory, in: .userDomainMask)
+    let url = urls.last?.appendingPathComponent("file.txt")
+    
+    do {
+      try "Hi there!".write(to: url!, atomically: true, encoding: .utf8)
+    } catch {
+      print("Error while writing")
+    }
+    
+    do {
+      let content = try String(contentsOf: url!, encoding: .utf8)
+      
+      if content == "Hi there!" {
+        print("Sucess :)")
+      } else {
+        print("Failed :(")
+      }
+    } catch {
+      print("Error while reading")
+    }
+  }
+}
